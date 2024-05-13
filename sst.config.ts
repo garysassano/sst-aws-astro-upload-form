c39349c724e1907c138b115b786c809c4670d202
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-05-13 16:08:41 +0200

    initial commit

---
+++ sst.config.ts
@@ -0,0 +1,20 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "sst-aws-astro",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    const bucket = new sst.aws.Bucket("MyBucket", {
+      public: true,
+    });
+
+    new sst.aws.Astro("MyWebsite", {
+      link: [bucket],
+    });
+  },
+});
